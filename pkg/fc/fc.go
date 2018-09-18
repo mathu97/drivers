d69c4d8c9becaac6b793d1294733e0ee0715f9d0
Mathusan Selvarajah <mselvara@redhat.com>
2018-09-18 16:03:40 -0400

    refactoring to enable testing

--- pkg/fc/fc.go
+++ pkg/fc/fc.go
@@ -6,12 +6,23 @@ import (
 	"github.com/container-storage-interface/spec/lib/go/csi/v0"
 	"github.com/mathu97/csi-connectors/fibrechannel"
 	"k8s.io/kubernetes/pkg/util/mount"
-	"k8s.io/kubernetes/pkg/volume/util"
+	volumeutil "k8s.io/kubernetes/pkg/volume/util"
 )
 
 type fcDevice struct {
 	connector *fibrechannel.Connector
 	disk      string
+}
+
+//FCMounter struct holds required parameters to mount a Fibre Channel Disk
+type FCMounter struct {
+	ReadOnly     bool
+	FsType       string
+	MountOptions []string
+	Mounter      *mount.SafeFormatAndMount
+	Exec         mount.Exec
+	DeviceUtil   volumeutil.DeviceUtil
+	TargetPath   string
 }
 
 func getFCInfo(req *csi.NodePublishVolumeRequest) (*fcDevice, error) {
@@ -49,17 +60,17 @@ func getFCInfo(req *csi.NodePublishVolumeRequest) (*fcDevice, error) {
 
 }
 
-func getFCDiskMounter(req *csi.NodePublishVolumeRequest) *fibrechannel.FCMounter {
+func getFCDiskMounter(req *csi.NodePublishVolumeRequest) *FCMounter {
 	readOnly := req.GetReadonly()
 	fsType := req.GetVolumeCapability().GetMount().GetFsType()
 	mountOptions := req.GetVolumeCapability().GetMount().GetMountFlags()
-	return &fibrechannel.FCMounter{
+	return &FCMounter{
 		ReadOnly:     readOnly,
 		FsType:       fsType,
 		MountOptions: mountOptions,
 		Mounter:      &mount.SafeFormatAndMount{Interface: mount.New(""), Exec: mount.NewOsExec()},
 		Exec:         mount.NewOsExec(),
-		DeviceUtil:   util.NewDeviceHandler(util.NewIOHandler()),
+		DeviceUtil:   volumeutil.NewDeviceHandler(volumeutil.NewIOHandler()),
 		TargetPath:   req.GetTargetPath(),
 	}
 }
--- pkg/fc/nodeserver.go
+++ pkg/fc/nodeserver.go
@@ -5,6 +5,7 @@ import (
 	"github.com/kubernetes-csi/drivers/pkg/csi-common"
 	"github.com/mathu97/csi-connectors/fibrechannel"
 	"golang.org/x/net/context"
+	"fmt"
 )
 
 type fcNodeServer struct {
@@ -29,13 +30,13 @@ func (ns *fcNodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePubl
 		return nil, err
 	}
 	fcDisk = *fcDevice
-	disk, connectError := fibrechannel.Connect(*fcDevice.connector)
+	disk, connectError := fibrechannel.Connect(*fcDevice.connector, nil)
 
 	fcDisk.disk = disk
 
 	//Mount
 	fcmounter := getFCDiskMounter(req)
-	fibrechannel.MountDisk(*fcmounter, disk)
+	MountDisk(*fcmounter, disk)
 
 	if connectError != nil {
 		return nil, connectError
@@ -46,7 +47,7 @@ func (ns *fcNodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePubl
 
 func (ns *fcNodeServer) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpublishVolumeRequest) (*csi.NodeUnpublishVolumeResponse, error) {
 
-	if err := fibrechannel.Disconnect(fcDisk.disk); err != nil {
+	if err := fibrechannel.Disconnect(fcDisk.disk, nil); err != nil {
 		return nil, err
 	}
 
@@ -77,4 +78,24 @@ func (ns *fcNodeServer) NodeGetInfo(ctx context.Context, req *csi.NodeGetInfoReq
 	return &csi.NodeGetInfoResponse{
 		NodeId: ns.Driver.nodeID,
 	}, nil
+}
+
+//MountDisk mounts the fibre channel disk with the given parameters
+func MountDisk(mnter FCMounter, devicePath string) error {
+	mntPath := mnter.TargetPath
+	notMnt, err := mnter.Mounter.IsLikelyNotMountPoint(mntPath)
+
+	if err != nil {
+		return fmt.Errorf("Heuristic determination of mount point failed: %v", err)
+	}
+
+	if !notMnt {
+		fmt.Printf("fc: %s already mounted", mnter.TargetPath)
+	}
+
+	if err = mnter.Mounter.FormatAndMount(devicePath, mnter.TargetPath, mnter.FsType, nil); err != nil {
+		return fmt.Errorf("fc: failed to mount fc volume %s [%s] to %s, error %v", devicePath, mnter.FsType, mnter.TargetPath, err)
+	}
+
+	return nil
 }
